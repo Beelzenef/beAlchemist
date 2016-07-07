07bf673fe91ed8467c7cecabd9ac1e80d8c28b42
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-07-07 17:12:48 +0200

    Simplificando GUI

--- GeneradorPociones/Form1.Designer.cs
+++ GeneradorPociones/Form1.Designer.cs
@@ -53,7 +53,7 @@
             // 
             // btn_GenPocion
             // 
-            this.btn_GenPocion.Location = new System.Drawing.Point(12, 256);
+            this.btn_GenPocion.Location = new System.Drawing.Point(27, 211);
             this.btn_GenPocion.Name = "btn_GenPocion";
             this.btn_GenPocion.Size = new System.Drawing.Size(101, 35);
             this.btn_GenPocion.TabIndex = 0;
@@ -64,16 +64,16 @@
             // lsB_Resultados
             // 
             this.lsB_Resultados.FormattingEnabled = true;
-            this.lsB_Resultados.Location = new System.Drawing.Point(119, 35);
+            this.lsB_Resultados.Location = new System.Drawing.Point(145, 31);
             this.lsB_Resultados.Name = "lsB_Resultados";
-            this.lsB_Resultados.Size = new System.Drawing.Size(421, 303);
+            this.lsB_Resultados.Size = new System.Drawing.Size(421, 277);
             this.lsB_Resultados.TabIndex = 1;
             // 
             // picB_Pocima
             // 
             this.picB_Pocima.Image = global::GeneradorPociones.recursos.pxA_pocion;
             this.picB_Pocima.InitialImage = null;
-            this.picB_Pocima.Location = new System.Drawing.Point(12, 35);
+            this.picB_Pocima.Location = new System.Drawing.Point(12, 44);
             this.picB_Pocima.Name = "picB_Pocima";
             this.picB_Pocima.Size = new System.Drawing.Size(101, 106);
             this.picB_Pocima.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -84,7 +84,7 @@
             // 
             this.grB_Contador.Controls.Add(this.lbl_NPociones);
             this.grB_Contador.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grB_Contador.Location = new System.Drawing.Point(12, 297);
+            this.grB_Contador.Location = new System.Drawing.Point(27, 252);
             this.grB_Contador.Name = "grB_Contador";
             this.grB_Contador.Size = new System.Drawing.Size(101, 48);
             this.grB_Contador.TabIndex = 4;
@@ -108,7 +108,7 @@
             this.info_MenuItem});
             this.menuUsuario.Location = new System.Drawing.Point(0, 0);
             this.menuUsuario.Name = "menuUsuario";
-            this.menuUsuario.Size = new System.Drawing.Size(552, 28);
+            this.menuUsuario.Size = new System.Drawing.Size(577, 28);
             this.menuUsuario.TabIndex = 5;
             this.menuUsuario.Text = "menuStrip1";
             // 
@@ -144,21 +144,21 @@
             // genHTML_MenuItem
             // 
             this.genHTML_MenuItem.Name = "genHTML_MenuItem";
-            this.genHTML_MenuItem.Size = new System.Drawing.Size(152, 24);
+            this.genHTML_MenuItem.Size = new System.Drawing.Size(117, 24);
             this.genHTML_MenuItem.Text = "HTML";
             this.genHTML_MenuItem.Click += new System.EventHandler(this.genHTML_MenuItem_Click);
             // 
             // genXML_MenuItem
             // 
             this.genXML_MenuItem.Name = "genXML_MenuItem";
-            this.genXML_MenuItem.Size = new System.Drawing.Size(152, 24);
+            this.genXML_MenuItem.Size = new System.Drawing.Size(117, 24);
             this.genXML_MenuItem.Text = "XML";
             this.genXML_MenuItem.Click += new System.EventHandler(this.genXML_MenuItem_Click);
             // 
             // rTFToolStripMenuItem
             // 
             this.rTFToolStripMenuItem.Name = "rTFToolStripMenuItem";
-            this.rTFToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.rTFToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
             this.rTFToolStripMenuItem.Text = "RTF";
             this.rTFToolStripMenuItem.Click += new System.EventHandler(this.genRTF_MenuItem_Click);
             // 
@@ -187,15 +187,12 @@
             // 
             this.chkLSB_Elementos.FormattingEnabled = true;
             this.chkLSB_Elementos.Items.AddRange(new object[] {
-            "Contenedor",
-            "Detalles",
-            "Textura",
-            "Olor",
-            "Sabor",
-            "Etiqueta"});
+            "Contenedor y etiqueta",
+            "Olor, sabor y textura",
+            "Detalles"});
             this.chkLSB_Elementos.Location = new System.Drawing.Point(7, 156);
             this.chkLSB_Elementos.Name = "chkLSB_Elementos";
-            this.chkLSB_Elementos.Size = new System.Drawing.Size(106, 94);
+            this.chkLSB_Elementos.Size = new System.Drawing.Size(132, 49);
             this.chkLSB_Elementos.TabIndex = 6;
             this.chkLSB_Elementos.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chkLSB_Elementos_ItemCheck);
             // 
@@ -211,7 +208,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(552, 348);
+            this.ClientSize = new System.Drawing.Size(577, 325);
             this.Controls.Add(this.chkLSB_Elementos);
             this.Controls.Add(this.grB_Contador);
             this.Controls.Add(this.picB_Pocima);
