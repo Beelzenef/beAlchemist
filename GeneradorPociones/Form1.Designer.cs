eefa486e59706e535fd0458795ceee29cb79f6b8
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-07-10 18:35:09 +0200

    Modificando constructor

--- GeneradorPociones/Form1.Designer.cs
+++ GeneradorPociones/Form1.Designer.cs
@@ -44,16 +44,20 @@
             this.info_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sFD_GuardarXML = new System.Windows.Forms.SaveFileDialog();
             this.sFD_GuardarHTML = new System.Windows.Forms.SaveFileDialog();
-            this.chkLSB_Elementos = new System.Windows.Forms.CheckedListBox();
             this.sFD_GuardarRTF = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkB_CE = new System.Windows.Forms.CheckBox();
+            this.chkB_TOS = new System.Windows.Forms.CheckBox();
+            this.chkB_Detalles = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.picB_Pocima)).BeginInit();
             this.grB_Contador.SuspendLayout();
             this.menuUsuario.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_GenPocion
             // 
-            this.btn_GenPocion.Location = new System.Drawing.Point(27, 211);
+            this.btn_GenPocion.Location = new System.Drawing.Point(42, 249);
             this.btn_GenPocion.Name = "btn_GenPocion";
             this.btn_GenPocion.Size = new System.Drawing.Size(101, 35);
             this.btn_GenPocion.TabIndex = 0;
@@ -64,16 +68,16 @@
             // lsB_Resultados
             // 
             this.lsB_Resultados.FormattingEnabled = true;
-            this.lsB_Resultados.Location = new System.Drawing.Point(145, 31);
+            this.lsB_Resultados.Location = new System.Drawing.Point(162, 36);
             this.lsB_Resultados.Name = "lsB_Resultados";
-            this.lsB_Resultados.Size = new System.Drawing.Size(421, 277);
+            this.lsB_Resultados.Size = new System.Drawing.Size(478, 290);
             this.lsB_Resultados.TabIndex = 1;
             // 
             // picB_Pocima
             // 
             this.picB_Pocima.Image = global::GeneradorPociones.recursos.pxA_pocion;
             this.picB_Pocima.InitialImage = null;
-            this.picB_Pocima.Location = new System.Drawing.Point(12, 44);
+            this.picB_Pocima.Location = new System.Drawing.Point(42, 44);
             this.picB_Pocima.Name = "picB_Pocima";
             this.picB_Pocima.Size = new System.Drawing.Size(101, 106);
             this.picB_Pocima.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -84,7 +88,7 @@
             // 
             this.grB_Contador.Controls.Add(this.lbl_NPociones);
             this.grB_Contador.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grB_Contador.Location = new System.Drawing.Point(27, 252);
+            this.grB_Contador.Location = new System.Drawing.Point(42, 289);
             this.grB_Contador.Name = "grB_Contador";
             this.grB_Contador.Size = new System.Drawing.Size(101, 48);
             this.grB_Contador.TabIndex = 4;
@@ -108,7 +112,7 @@
             this.info_MenuItem});
             this.menuUsuario.Location = new System.Drawing.Point(0, 0);
             this.menuUsuario.Name = "menuUsuario";
-            this.menuUsuario.Size = new System.Drawing.Size(577, 28);
+            this.menuUsuario.Size = new System.Drawing.Size(652, 28);
             this.menuUsuario.TabIndex = 5;
             this.menuUsuario.Text = "menuStrip1";
             // 
@@ -183,24 +187,60 @@
             this.sFD_GuardarHTML.FileName = "HTML";
             this.sFD_GuardarHTML.Filter = "HTML | *.html | HTM | *.htm";
             // 
-            // chkLSB_Elementos
-            // 
-            this.chkLSB_Elementos.FormattingEnabled = true;
-            this.chkLSB_Elementos.Items.AddRange(new object[] {
-            "Contenedor y etiqueta",
-            "Olor, sabor y textura",
-            "Detalles"});
-            this.chkLSB_Elementos.Location = new System.Drawing.Point(7, 156);
-            this.chkLSB_Elementos.Name = "chkLSB_Elementos";
-            this.chkLSB_Elementos.Size = new System.Drawing.Size(132, 49);
-            this.chkLSB_Elementos.TabIndex = 6;
-            this.chkLSB_Elementos.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chkLSB_Elementos_ItemCheck);
-            // 
             // sFD_GuardarRTF
             // 
             this.sFD_GuardarRTF.DefaultExt = "rtf";
             this.sFD_GuardarRTF.FileName = "Fichero RTF";
             this.sFD_GuardarRTF.Filter = "RTF | *.RTF";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkB_Detalles);
+            this.groupBox1.Controls.Add(this.chkB_TOS);
+            this.groupBox1.Controls.Add(this.chkB_CE);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 156);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 87);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pociones";
+            // 
+            // chkB_CE
+            // 
+            this.chkB_CE.AutoSize = true;
+            this.chkB_CE.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkB_CE.Location = new System.Drawing.Point(11, 23);
+            this.chkB_CE.Name = "chkB_CE";
+            this.chkB_CE.Size = new System.Drawing.Size(130, 17);
+            this.chkB_CE.TabIndex = 1;
+            this.chkB_CE.Text = "Contenedor y etiqueta";
+            this.chkB_CE.UseVisualStyleBackColor = true;
+            this.chkB_CE.CheckedChanged += new System.EventHandler(this.chkB_CE_CheckedChanged);
+            // 
+            // chkB_TOS
+            // 
+            this.chkB_TOS.AutoSize = true;
+            this.chkB_TOS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkB_TOS.Location = new System.Drawing.Point(11, 46);
+            this.chkB_TOS.Name = "chkB_TOS";
+            this.chkB_TOS.Size = new System.Drawing.Size(120, 17);
+            this.chkB_TOS.TabIndex = 2;
+            this.chkB_TOS.Text = "Olor, sabor y textura";
+            this.chkB_TOS.UseVisualStyleBackColor = true;
+            this.chkB_TOS.CheckedChanged += new System.EventHandler(this.chkB_TOS_CheckedChanged);
+            // 
+            // chkB_Detalles
+            // 
+            this.chkB_Detalles.AutoSize = true;
+            this.chkB_Detalles.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkB_Detalles.Location = new System.Drawing.Point(11, 69);
+            this.chkB_Detalles.Name = "chkB_Detalles";
+            this.chkB_Detalles.Size = new System.Drawing.Size(64, 17);
+            this.chkB_Detalles.TabIndex = 3;
+            this.chkB_Detalles.Text = "Detalles";
+            this.chkB_Detalles.UseVisualStyleBackColor = true;
+            this.chkB_Detalles.CheckedChanged += new System.EventHandler(this.chkB_Detalles_CheckedChanged);
             // 
             // formPociones
             // 
@@ -208,8 +248,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(577, 325);
-            this.Controls.Add(this.chkLSB_Elementos);
+            this.ClientSize = new System.Drawing.Size(652, 341);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.grB_Contador);
             this.Controls.Add(this.picB_Pocima);
             this.Controls.Add(this.lsB_Resultados);
@@ -225,6 +265,8 @@
             this.grB_Contador.PerformLayout();
             this.menuUsuario.ResumeLayout(false);
             this.menuUsuario.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,9 +287,12 @@
         private System.Windows.Forms.ToolStripMenuItem info_MenuItem;
         private System.Windows.Forms.SaveFileDialog sFD_GuardarXML;
         private System.Windows.Forms.SaveFileDialog sFD_GuardarHTML;
-        private System.Windows.Forms.CheckedListBox chkLSB_Elementos;
         private System.Windows.Forms.ToolStripMenuItem rTFToolStripMenuItem;
         private System.Windows.Forms.SaveFileDialog sFD_GuardarRTF;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkB_Detalles;
+        private System.Windows.Forms.CheckBox chkB_TOS;
+        private System.Windows.Forms.CheckBox chkB_CE;
     }
 }
 
--- GeneradorPociones/Form1.cs
+++ GeneradorPociones/Form1.cs
@@ -29,13 +29,41 @@ namespace GeneradorPociones
             lsB_Resultados.Items.Add("Efecto: " + unaPocion.EfectoPrim);
             lsB_Resultados.Items.Add("Efecto secundario: " + unaPocion.EfectoSec);
             lsB_Resultados.Items.Add("Color: " + unaPocion.Color);
-            lsB_Resultados.Items.Add("Detalles: " + unaPocion.Detalle);
-            lsB_Resultados.Items.Add("Textura: " + unaPocion.Textura);
+
+            switch (marcandoOpciones)
+            {
+                case 1:
+                    lsB_Resultados.Items.Add("Un " + unaPocion.Contenedor + " que reza " + unaPocion.Etiqueta);
+                    break;
+                case 2:
+                    lsB_Resultados.Items.Add("Textura: " + unaPocion.Textura + ", olor: " + unaPocion.Olor + ", sabor: " + unaPocion.Sabor);
+                    break;
+                case 3:
+                    lsB_Resultados.Items.Add("Textura: " + unaPocion.Textura + ", olor: " + unaPocion.Olor + ", sabor: " + unaPocion.Sabor);
+                    lsB_Resultados.Items.Add("Un " + unaPocion.Contenedor + " que reza " + unaPocion.Etiqueta);
+                    break;
+                case 4:
+                    lsB_Resultados.Items.Add("Detalles: " + unaPocion.Detalle);
+                    break;
+                case 5:
+                    lsB_Resultados.Items.Add("Un " + unaPocion.Contenedor + " que reza " + unaPocion.Etiqueta);
+                    lsB_Resultados.Items.Add("Detalles: " + unaPocion.Detalle);
+                    break;
+                case 6:
+                    lsB_Resultados.Items.Add("Detalles: " + unaPocion.Detalle);
+                    lsB_Resultados.Items.Add("Textura: " + unaPocion.Textura + ", olor: " + unaPocion.Olor + ", sabor: " + unaPocion.Sabor);
+                    break;
+                case 7:
+                    lsB_Resultados.Items.Add("Un " + unaPocion.Contenedor + " que reza " + unaPocion.Etiqueta);
+                    lsB_Resultados.Items.Add("Detalles: " + unaPocion.Detalle);
+                    lsB_Resultados.Items.Add("Textura: " + unaPocion.Textura + ", olor: " + unaPocion.Olor + ", sabor: " + unaPocion.Sabor);
+                    break;
+            }
 
             lsB_Resultados.Items.Add("");
-
             listaPoc.Add(unaPocion);
         }
+
 
         #region Gestion de menu
 
@@ -64,7 +92,7 @@ namespace GeneradorPociones
             sFD_GuardarXML.FileName = "Fichero XML";
         }
 
-        
+
         private void genHTML_MenuItem_Click(object sender, EventArgs e)
         {
             if (sFD_GuardarHTML.ShowDialog() != DialogResult.OK)
@@ -102,34 +130,32 @@ namespace GeneradorPociones
 
         #endregion
 
-        #region Modificando constructor
+        #region Modificando constructor        
 
-        bool incluirTextura = false;
-        bool incluirOlor = false;
-        bool incluirSabor = false;
-        bool incluirEtiqueta = false;
-        bool incluirContenedor = false;
-        bool incluirDetalles = false;
-
-        private void chkLSB_Elementos_ItemCheck(object sender, ItemCheckEventArgs e)
+        private void chkB_CE_CheckedChanged(object sender, EventArgs e)
         {
-            if (chkLSB_Elementos.SelectedIndex == 0)
-                incluirContenedor = !incluirContenedor;
-            if (chkLSB_Elementos.SelectedIndex == 1)
-                incluirDetalles = !incluirDetalles;
-            if (chkLSB_Elementos.SelectedIndex == 2)
-                incluirTextura = !incluirTextura;
-            if (chkLSB_Elementos.SelectedIndex == 3)
-                incluirOlor = !incluirOlor;
-            if (chkLSB_Elementos.SelectedIndex == 4)
-                incluirSabor = !incluirSabor;
-            if (chkLSB_Elementos.SelectedIndex == 5)
-                incluirEtiqueta = !incluirEtiqueta;
+            if (chkB_CE.Checked)
+                marcandoOpciones += 1;
+            else
+                marcandoOpciones -= 1;
         }
 
+        private void chkB_TOS_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkB_TOS.Checked)
+                marcandoOpciones += 2;
+            else
+                marcandoOpciones -= 2;
+        }
 
+        private void chkB_Detalles_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkB_Detalles.Checked)
+                marcandoOpciones += 4;
+            else
+                marcandoOpciones -= 4;
+        }
 
         #endregion
-
     }
 }
